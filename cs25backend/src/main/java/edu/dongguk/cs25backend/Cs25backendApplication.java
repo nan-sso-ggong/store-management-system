b06db25dac5f1a366a518b5f4ef47cc881830b2c
kimwiseong <kwws919@gmail.com>
2023-11-17 11:44:37 +0900

    Initial Commit

---
+++ cs25backend/src/main/java/edu/dongguk/cs25backend/Cs25backendApplication.java
@@ -0,0 +1,13 @@
+package edu.dongguk.cs25backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs25backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs25backendApplication.class, args);
+    }
+
+}
---
+++ cs25backend/src/test/java/edu/dongguk/cs25backend/Cs25backendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dongguk.cs25backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs25backendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
