40eb126b07d3867f203bb73638d4842819af16eb
Kang Woobin <woobin929@dgu.ac.kr>
2023-11-24 01:04:01 +0900

    feat : 점주, 주문, 점포 엔티티 추가

---
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/domain/OrderItemCS.kt
@@ -0,0 +1,30 @@
+package edu.dongguk.cs25server.domain
+
+import jakarta.persistence.*
+import org.hibernate.annotations.DynamicUpdate
+
+@Entity
+@DynamicUpdate
+@Table(name = "order_item_cs")
+class OrderItemCS(
+        var orderPrice: Int,
+        var count: Int,
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "item_cs_id")
+        var itemCS: ItemCS,
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "order_id")
+        var order: Order
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_item_id")
+    val id: Long? = null
+
+    fun cancel() {
+        this.itemCS.addStock(count)
+    }
+
+}
+
+
