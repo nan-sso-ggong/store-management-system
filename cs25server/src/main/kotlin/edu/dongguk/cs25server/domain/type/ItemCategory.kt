0cbd8cfbaf549f4fb9f9f450d18d08dd100e9733
prefer52 <tjsgh151@naver.com>
2023-11-24 18:29:23 +0900

    feat: 보유 재고 목록 조회 API 구현 완료

--- cs25server/src/main/kotlin/edu/dongguk/cs25server/domain/type/ItemCategory.kt
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/domain/type/ItemCategory.kt
@@ -1,9 +1,26 @@
 package edu.dongguk.cs25server.domain.type
 
+import edu.dongguk.cs25server.exception.ErrorCode
+import edu.dongguk.cs25server.exception.GlobalException
+
 enum class ItemCategory(private val category: String) {
     ICE_CREAM("아이스크림"), SNACK("과자"), NOODLE("라면"), BEVERAGE("음료");
 
     override fun toString(): String {
         return category
+    }
+
+    companion object {
+        fun getCategory(category: String?): ItemCategory? {
+            if (category.isNullOrBlank())
+                throw GlobalException(ErrorCode.WRONG_CATEGORY_ERROR)
+
+            for(value in ItemCategory.values()) {
+                if (category.equals(value.category)) {
+                    return value
+                }
+            }
+            return null
+        }
     }
 }
--- cs25server/src/main/kotlin/edu/dongguk/cs25server/dto/response/StockResponseDto.kt
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/dto/response/StockResponseDto.kt
@@ -1,9 +1,11 @@
 package edu.dongguk.cs25server.dto.response
 
+import java.sql.Timestamp
+
 class StockResponseDto(
-    val item_id: Long,
+    val item_id: Long?,
     val item_name: String,
-    val selling_price: Long,
+    val supply_price: Long,
     val stock_quantity: Long,
-    val warehousing_date: String
+    val warehousing_date: Timestamp
 )
