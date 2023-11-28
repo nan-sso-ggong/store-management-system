16bdd863af35382fc90556d0ff1020f7d48c3914
kimwiseong <kwws919@gmail.com>
2023-11-28 11:25:18 +0900

    feat : 상품 결제 / 주문 내역 조회/ 환불 처리

--- cs25server/src/main/kotlin/edu/dongguk/cs25server/repository/OrderRepository.kt
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/repository/OrderRepository.kt
@@ -12,13 +12,16 @@ import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import java.time.LocalDateTime
 
-interface OrderRepostiory: JpaRepository<Order, Long> {
-    fun findAllByStore(store: Store, pageable: Pageable): Page<Order>?
-
-    @Query(value = "SELECT o.customer.name AS CNAME, o.id AS OID, oi.itemCS.name AS INAME, o.finalPrice AS PRICE, o.createdAt AS CREATE, o.orderStatus AS STATUS " +
-            "FROM Order AS o INNER JOIN OrderItemCS AS oi ON oi.order = o WHERE o.store = :store",
-    countQuery = "SELECT count(*) FROM Order")
-    fun findCustomerOrderByStore(@Param("store") store: Store, pageable: Pageable): Page<CustomerOrderInfo>
+interface OrderRepository: JpaRepository<Order, Long> {
+    @Query(value = "SELECT DISTINCT o.customer.name AS CNAME, o.id AS OID, " +
+            "(SELECT GROUP_CONCAT(oi.itemCS.name) FROM OrderItemCS oi WHERE oi.order = o) AS INAME," +
+            "o.finalPrice AS PRICE, o.createdAt AS CREATE, o.orderStatus AS STATUS " +
+            "FROM Order AS o INNER JOIN OrderItemCS AS oi ON oi.order = o " +
+            "WHERE o.store = :store AND (:state IS NULL OR o.orderStatus = :state) " +
+            "AND (:startDate IS NULL OR o.createdAt >= :startDate) " +
+            "AND (:endDate IS NULL OR o.createdAt <= :endDate)",
+    countQuery = "SELECT count(o) FROM Order AS o WHERE o.store = :store")
+    fun findCustomerOrderByStore(@Param("store") store: Store, @Param("state") state: OrderStatus?, @Param("startDate") starteDate: LocalDateTime?, @Param("endDate") endDateTime: LocalDateTime?, pageable: Pageable): Page<CustomerOrderInfo>
 
     fun findByCustomer(customer: Customer, paging: Pageable): Page<Order>
 
