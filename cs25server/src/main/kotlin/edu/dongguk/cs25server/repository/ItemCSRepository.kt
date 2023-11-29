57d1cc6bdd47243d8e0bdacb33df52994f0c1f8f
nicolao00 <nicolao00@naver.com>
2023-11-29 18:21:59 +0900

    feat: 점주-발주 목록 조회 API 구현

--- cs25server/src/main/kotlin/edu/dongguk/cs25server/repository/ItemCSRepository.kt
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/repository/ItemCSRepository.kt
@@ -24,4 +24,15 @@ interface ItemCSRepository : JpaRepository<ItemCS, Long> {
     @Query("SELECT i FROM ItemCS i WHERE i.store.id = :storeId AND i.id IN (:ids)")
     fun findByIdsAndStore(storeId: Long, ids: List<Long>): List<ItemCS>
 
+    @Query("SELECT ih.id AS IHID, ic.id AS ICID, ic.name AS ICNAME, ih.category AS IHCG, ic.stock AS ICST, ih.price AS ICPR FROM ItemCS AS ic INNER JOIN ItemHQ AS ih ON ic.itemHQ = ih WHERE ic.store = :store AND (ic.name LIKE %:keyword% OR :keyword IS NULL) AND (ih.category = :category OR :category IS NULL)")
+    fun findAllByKeywordAndCategory(@Param("store") store: Store, @Param("keyword") keyword: String?, @Param("category") category: ItemCategory?, pageable: Pageable): Page<OrderItemInfo>
+
+    interface OrderItemInfo {
+        fun getIHID(): Long
+        fun getICID(): Long
+        fun getICNAME(): String
+        fun getIHCG(): ItemCategory
+        fun getICST(): Int
+        fun getICPR(): Int
+    }
 }
---
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/service/OrderCSService.kt
@@ -0,0 +1,52 @@
+package edu.dongguk.cs25server.service
+
+import edu.dongguk.cs25server.domain.Store
+import edu.dongguk.cs25server.domain.type.ItemCategory
+import edu.dongguk.cs25server.dto.response.ListResponseDto
+import edu.dongguk.cs25server.dto.response.OrderItemResponseDto
+import edu.dongguk.cs25server.dto.response.PageInfo
+import edu.dongguk.cs25server.exception.ErrorCode
+import edu.dongguk.cs25server.exception.GlobalException
+import edu.dongguk.cs25server.repository.ItemCSRepository
+import edu.dongguk.cs25server.repository.ItemHQRepository
+import edu.dongguk.cs25server.repository.OrderApplicationRepository
+import edu.dongguk.cs25server.repository.StoreRepository
+import edu.dongguk.cs25server.util.Log.Companion.log
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class OrderCSService(private val storeRepository: StoreRepository, private val itemHQRepository: ItemHQRepository, private val orderApplicationRepository: OrderApplicationRepository, private val itemCSRepository: ItemCSRepository) {
+    // 발주 목록 조회
+    fun readItemOrders(storeId: Long, keyword: String?, category: ItemCategory?, pageIndex: Long): ListResponseDto<List<OrderItemResponseDto>> {
+        val store: Store = storeRepository.findByIdOrNull(storeId) ?: throw GlobalException(ErrorCode.NOT_FOUND_STORE)
+        val paging = PageRequest.of(
+            pageIndex.toInt(),
+            10
+        )
+        val itemCSList: Page<ItemCSRepository.OrderItemInfo> = itemCSRepository.findAllByKeywordAndCategory(store, keyword, category, paging)
+        val pageInfo: PageInfo = PageInfo(
+            page = pageIndex.toInt(),
+            size = 10,
+            totalElements = itemCSList.totalElements.toInt(),
+            totalPages = itemCSList.totalPages
+        )
+
+        val orderItemResponseDto: List<OrderItemResponseDto> = itemCSList.map {
+            item -> OrderItemResponseDto(
+                item.getICID(),
+                item.getICNAME(),
+                item.getIHCG(),
+                item.getICST(),
+                item.getICPR(),
+                orderApplicationRepository.existsByStoreAndItemHQ(store, itemHQRepository.findById(item.getIHID()).get())
+            )
+        }.toList()
+        return ListResponseDto(orderItemResponseDto, pageInfo)
+    }
+}
