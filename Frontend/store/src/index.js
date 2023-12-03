c61b42ab70aff5884e4a20ba5e4394f7424f989c
park20011029 <rokmck156@gmail.com>
2023-12-04 00:14:55 +0900

    feat: 점포 값 받아오기

--- Frontend/store/src/index.js
+++ Frontend/store/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+    </Provider>
 );
