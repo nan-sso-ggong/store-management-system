1a0c7333ae04e69aa89138d4e926444cbaf43a8f
park20011029 <rokmck156@gmail.com>
2023-12-04 00:18:38 +0900

    feat: axios 헤더에 token 넣기

---
+++ Frontend/store/src/Axios.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://13.125.112.60:8080/api/v1', // API의 기본 URL 설정
+    headers: {
+        'Content-Type': 'application/json', // 요청의 Content-Type 설정
+        Authorization: `Bearer ${localStorage.getItem('access_token')}`
+    },
+});
+
+const reissueAccessToken = async () => {
+    try {
+        const refreshToken = localStorage.getItem('refresh_token');
+        const response = await axios.post('/auth/customers/reissue', null, {
+            headers: {
+                'Authorization': `Bearer ${refreshToken}`
+            }
+        });
+
+        if (response.data.success) {
+            const { access_token } = response.data.data;
+            localStorage.setItem('access_token', access_token);
+            // 토큰 재발급 후, axios의 기본 headers를 재설정
+            api.defaults.headers['Authorization'] = `Bearer ${access_token}`;
+        } else {
+            console.error(response.data.error);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+api.interceptors.response.use(
+    response => response,
+    async error => {
+        const originalRequest = error.config;
+        if (error.response.status === 433 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            await reissueAccessToken();
+            return api(originalRequest);
+        }
+        return Promise.reject(error);
+    }
+);
+
+export default api;
