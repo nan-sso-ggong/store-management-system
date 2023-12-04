0f5322c8a0e22c698a6047ef9ea3fbb817a9a9c9
nicolao00 <nicolao00@naver.com>
2023-12-04 21:44:48 +0900

    feat: totalPrice 및 오탈자 수정

--- cs25server/src/main/kotlin/edu/dongguk/cs25server/repository/WarehousingApplicationRepository.kt
+++ cs25server/src/main/kotlin/edu/dongguk/cs25server/repository/WarehousingApplicationRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface WarehousingAppliactionRepository: JpaRepository<WarehousingApplication, Long> {
+interface WarehousingApplicationRepository: JpaRepository<WarehousingApplication, Long> {
 }
